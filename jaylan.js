f8bb61eae02c5faee36238a2316faee16f1a0764
jaylan-wu <jw6639@nyu.edu>
2022-02-25 23:13:54 -0500

    test react code
    test react code

---
+++ jaylan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const yourApp = () => {
+    return (
+        <View style={{ flex: 1, justifyConcent: "center", alignItems: "center"}}>
+        <Text>
+            Try editing me!
+        </Text>
+        </View>
+    );
+}
