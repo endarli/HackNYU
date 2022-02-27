901e733200f9d797e23de7268396509bd748daaa
alexanderleong1 <99528025+alexanderleong1@users.noreply.github.com>
2022-02-26 22:17:42 -0500

    Add files via upload
    Added JavaDoc style documentation for methods. Changed return type values for each method. Now accepts different files as input.

--- src/PriceParser.js
+++ src/PriceParser.js
@@ -1,28 +1,64 @@
-Tesseract.recognize(
-    'Capture6.png',
-    'eng',
- { logger: m => console.log(m) }
-).then(({ data: { text } }) => {
-    console.log(text);
-    const wordArr = text.split(/\s+/);
-    find_total(wordArr);
-}).catch(err=>{
-    console.log(err);
-})
+/**
+ * Price parser from receipt image. Built using Tesseract.js library.
+*/
 
+/**
+ * Utilizes the Tesseract.js OCR to translate text in an image into words.
+ * After the text has been converted into a string, the string is split by whitespace characters,
+ * and stored in a string array. This string array is then passed as an argument
+ * to methods that search for 
+ * 
+ * @param fileName - The file name of the receipt we are reading the total amount from. Must be passed as a String.
+ * 
+ * @return returns the floating-point value of the total parsed from the receipt.
+ */
+
+parseText("Test1.png");
+
+function parseText(fileName) {
+    Tesseract.recognize(
+        fileName,
+        'eng',
+     { logger: m => console.log(m) }
+    ).then(({ data: { text } }) => {
+        console.log(text);
+        const wordArr = text.split(/\s+/);
+        const result = find_total(wordArr);
+        return result;
+    }).catch(err=>{
+        console.log(err);
+        throw (err);
+    })
+}
+
+/**
+ * Using an array of strings, finds the last instance of words that indicate "total amount of money".
+ * Passes this index to a helper function, along with a sliced portion of the array from the index onwards.
+ * 
+ * @param {*} wordArr - the string of all words from our Tesseract OCR, split into an array of strings.
+ * 
+ * @return the result of finding the floating-point value of the total read from the receipt.
+ */
 function find_total(wordArr) {
+    var i;
     for(i = wordArr.length-1; i >=0; i--) {
         var formatter = wordArr[i].toLowerCase().replace(/\s/g, "");
         if(formatter.includes("total") || formatter.includes("balance") || formatter.includes("due") || formatter.includes("amount")) {
             // Then we pass the array sliced from formatter onwards to helper function.
             wordArr = wordArr.slice(i);
-            find_total_helper(wordArr);
+            return find_total_helper(wordArr);
             // document.getElementById("textfield").innerHTML+=wordArr[i+1];
-            break;
         }
     }
 }
 
+/**
+ * Using a preprocessed string of arrays, finds the instance that best matches the floating-point value for the grand total.
+ * 
+ * @param {*} wordArr - string array of words parsed from the receipt by Tesseract OCR. Preprocessed by find_total().
+ * 
+ * @return a floating-point value containing the total amount read from the receipt. Bubbles up the call stack to parseText().
+ */
 function find_total_helper(wordArr) {
     for(i = 0; i < wordArr.length; i++) {
         console.log(wordArr[i]);
@@ -32,7 +68,7 @@ function find_total_helper(wordArr) {
         let checkIfFloat = parseFloat(wordArr[i]);
         if(!isNaN(checkIfFloat) && checkIfFloat != "" && checkIfFloat != " ") { // First instance of a floating-point after one of the "total" key words
             document.getElementById("textfield").innerHTML+=wordArr[i]; // Logs result of scan directly to the web page.
-            break;
+            return wordArr[i];
         }
     }
 }
