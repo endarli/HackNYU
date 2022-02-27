d75304f1b3664403fdfcc31749544278332fff43
jaylan-wu <jw6639@nyu.edu>
2022-02-26 20:45:41 -0500

    included labels pi graph

--- src/App.js
+++ src/App.js
@@ -4,22 +4,41 @@ import './App.css';
 
 function App() {
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#FF8042'];
+  const COLORS = ['#2085EC', '#72B4EB', '#0E59AA', '#8464A0', '#F9CFF2'];
+
+  var food = 323.00;
+  var clothing = 223.42;
+  var appliances = 25.03;
+  var entertainment = 400.32;
+  var other = 62.34;
 
   const data = [
-    {name: 'food', cost: 323.40},
-    {name: 'clothing', cost: 100.40},
-    {name: 'appliances', cost: 42.40},
-    {name: 'appliances', cost: 3.40},
-    {name: 'other', cost: 452.40}
+    {name: 'Food', cost: food},
+    {name: 'Clothing', cost: clothing},
+    {name: 'Appliances', cost: appliances},
+    {name: 'Entertainment', cost: entertainment},
+    {name: 'Other', cost: other}
   ]
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 1.3;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill={COLORS[index % COLORS.length]} fontFamily='verdana' fontSize={24} textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {data[index].name + ": $" + data[index].cost.toFixed(2)}
+      </text>
+    );
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <PieChart width={700} height={700}>
-          <Pie data={data} dataKey="cost" outerRadius={250} fill="8884d8">
-              {data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)}
+        <PieChart width={950} height={600}>
+          <Pie data={data} dataKey="cost" isAnimationActive={true} innerRadius={100} outerRadius={200} fill="8884d8" label={renderCustomizedLabel}>
+              {data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)};
           </Pie>
         </PieChart>
       </header>
